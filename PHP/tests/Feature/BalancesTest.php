242f5e02c5756bdf0196108f87bcd32f3e40171d
Alexey Demin <deminn@gmail.com>
2017-09-04 23:04:02 -0500

    Tests refactored

--- PHP/tests/Feature/BalancesTest.php
+++ PHP/tests/Feature/BalancesTest.php
@@ -7,29 +7,28 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class CheckBalanceBetweenTransactionsTest extends TestCase
+class BalancesTest extends TestCase
 {
+    protected $period, $userIds, $part, $content;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+
+        $this->period = $this->createDateRange('2017-07-01', '2017-07-14');
+        $this->userIds = range(1, 4);
+    }
+
     public function testCheckSumsInsideDay()
     {
-        $period = new \DatePeriod(
-            new \DateTime('2017-07-01'),
-            new \DateInterval('P1D'),
-            new \DateTime('2017-07-14')
-        );
 
-        foreach(range(1, 4) as $userId) {
-            foreach ($period as $date) {
-                $part = 'credit';
-                $date = $date->format('Y-m-d');
-                $content = $this->get("/api/transactions/json/$userId/$date")->decodeResponseJson();
-                $totalDebitJSON = ltrim($content['totals'][$part], '$');
-                if ($totalDebitJSON == 0) {
-                    $part = 'debit';
-                }
-                $totalDebitJSON = ltrim($content['totals'][$part], '$');
+        foreach($this->userIds as $userId) {
+            foreach ($this->period as $date) {
+                $this->detectTransactionsType($userId, $date);
+                $totalDebitJSON = rmSign($this->content['totals'][$this->part]);
                 $totalDebitCalculated = 0;
-                foreach ($content['transactions'] as $transaction) {
-                    $totalDebitCalculated += ltrim($transaction[$part], '$');
+                foreach ($this->content['transactions'] as $transaction) {
+                    $totalDebitCalculated += rmSign($transaction[$this->part]);
                 }
 
                 $observationalError = abs($totalDebitJSON - $totalDebitCalculated) / $totalDebitCalculated * 100;
@@ -42,33 +41,16 @@ class CheckBalanceBetweenTransactionsTest extends TestCase
 
     public function testCheckSumsBetweenDays()
     {
-        $periodObj = new \DatePeriod(
-            new \DateTime('2017-07-01'),
-            new \DateInterval('P1D'),
-            new \DateTime('2017-07-14')
-        );
-        $period = [];
-        foreach($periodObj as $date) { $period[] = $date->format('Y-m-d'); }
-
-        foreach(range(1, 4) as $userId) {
-            foreach ($period as $key => $date) {
-                if(empty($period[$key+1])){
+        foreach($this->userIds as $userId) {
+            foreach ($this->period as $key => $date) {
+                if(empty($this->period[$key+1])){
                     continue;
                 }
-
-                $part = 'credit';
-                $content = $this->get("/api/transactions/json/$userId/$date")->decodeResponseJson();
-                $totalDebitJSON = ltrim($content['totals'][$part], '$');
-                if ($totalDebitJSON == 0) {
-                    $part = 'debit';
-                }
-
-                $balancePrev = ltrim(end($content['transactions'])['balance'], '$');
-                $this->refreshApplication();
-
-                $content = $this->get("/api/transactions/json/$userId/{$period[$key+1]}")->decodeResponseJson();
-                $debit = ltrim($content['transactions'][0][$part], '$');
-                $balance = ltrim($content['transactions'][0]['balance'], '$');
+                $this->detectTransactionsType($userId, $date);
+                $balancePrev = rmSign(end($this->content['transactions'])['balance']);
+                $this->content = $this->get("/api/transactions/json/$userId/{$this->period[$key+1]}")->decodeResponseJson();
+                $debit = rmSign($this->content['transactions'][0][$this->part]);
+                $balance = rmSign($this->content['transactions'][0]['balance']);
 
                 $diff = abs($balancePrev + $debit - $balance);
                 echo "\n[$userId][$date][$key] $debit + $balancePrev = $balance | $diff";
@@ -81,29 +63,17 @@ class CheckBalanceBetweenTransactionsTest extends TestCase
 
     public function testCheckBalanceBetweenTransactions()
     {
-        $period = new \DatePeriod(
-            new \DateTime('2017-07-01'),
-            new \DateInterval('P1D'),
-            new \DateTime('2017-07-14')
-        );
 
-        foreach(range(1, 4) as $userId) {
-            foreach ($period as $date) {
-                $part = 'credit';
-                $date = $date->format('Y-m-d');
-                $content = $this->get("/api/transactions/json/$userId/$date")->decodeResponseJson();
-                $totalDebitJSON = ltrim($content['totals'][$part], '$');
-                if ($totalDebitJSON == 0) {
-                    $part = 'debit';
-                }
-
-                foreach ($content['transactions'] as $key => $transaction) {
-                    if(!isset($content['transactions'][$key-1])){
+        foreach($this->userIds as $userId) {
+            foreach ($this->period as $date) {
+                $this->detectTransactionsType($userId, $date);
+                foreach ($this->content['transactions'] as $key => $transaction) {
+                    if(!isset($this->content['transactions'][$key-1])){
                         continue;
                     }
-                    $debit = ltrim($transaction[$part], '$');
-                    $balance = ltrim($transaction['balance'], '$');
-                    $balancePrev = ltrim($content['transactions'][$key-1]['balance'], '$');
+                    $debit = rmSign($transaction[$this->part]);
+                    $balance = rmSign($transaction['balance']);
+                    $balancePrev = rmSign($this->content['transactions'][$key-1]['balance']);
                     $diff = abs($balancePrev + $debit - $balance);
                     echo "\n[$userId][$date][$key] $debit + $balancePrev = $balance | $diff";
                     $this->assertTrue($diff <= 0.011);
@@ -117,8 +87,39 @@ class CheckBalanceBetweenTransactionsTest extends TestCase
         echo "\n\n";
     }
 
+    protected function createDateRange($startDate, $endDate, $format = "Y-m-d")
+    {
+        $begin = new \DateTime($startDate);
+        $end = new \DateTime($endDate);
+
+        $interval = new \DateInterval('P1D'); // 1 Day
+        $dateRange = new \DatePeriod($begin, $interval, $end);
+
+        $range = [];
+        foreach ($dateRange as $date) {
+            $range[] = $date->format($format);
+        }
+
+        return $range;
+    }
+
+    protected function detectTransactionsType($userId, $date)
+    {
+        $this->part = 'credit';
+        $this->content = $this->get("/api/transactions/json/$userId/$date")->decodeResponseJson();
+        $totalDebitJSON = rmSign($this->content['totals'][$this->part]);
+        if ($totalDebitJSON == 0) {
+            $this->part = 'debit';
+        }
+        $this->content = $this->get("/api/transactions/json/$userId/$date")->decodeResponseJson();
+        $this->refreshApplication();
+    }
 
 
 
+}
 
+function rmSign($v)
+{
+    return ltrim($v, '$');
 }
